31f2be4fd690351e939c4686a27c5ab76fe3d26a
Alex Lomas <alexlomas951@gmail.com>
2024-11-20 11:05:41 +0800

    Updated setup, stop and forwards functions to control a single motor and created a test sequence for these functions; removed shutdown function as it was not necessary

--- driving_motor.c
+++ driving_motor.c
@@ -1,33 +1,38 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+#define MOTOR_A_PIN_1 3 // IN1 (Forward for Motor A)
+#define MOTOR_A_PIN_2 4 // IN2 (Backward for Motor A)
+
 void driving_motor_setup() {
     // Sets up the driving motor.
-    printf("Setting up driving motor...\n");
+    printf("Setting up the driving motor.");
+    gpio_init(MOTOR_A_PIN_1);
+    gpio_set_dir(MOTOR_A_PIN_1, GPIO_OUT);
+
+    gpio_init(MOTOR_A_PIN_2);
+    gpio_set_dir(MOTOR_A_PIN_2, GPIO_OUT);
 }
 
 int driving_motor_stop() {
-    // Sets speed of driving motor to 0, but does not shut down motor.
-    printf("Setting motor speed to zero...\n");
+    // Stops driving motor.
+    gpio_put(MOTOR_A_PIN_1, 0);
+    gpio_put(MOTOR_A_PIN_2, 0);
+
     int new_speed = 0;
     return new_speed;
 }
 
-void driving_motor_shutdown() {
-    // Shuts down driving motor. 
-    driving_stop(); // is it okay that this function returns a value, but it is being treated
-    // as a procedure?
-    printf("Shutting down driving motor...\n");
-}
-
 int drive_forwards() {
     // Turns on the driving motor, with the motor speed value (0-255) initialised to 50.
-    printf("Setting driving motor to ON\n");
+    gpio_put(MOTOR_A_PIN_1, 1);
+    gpio_put(MOTOR_A_PIN_2, 0);
+
     int new_speed = 50; // Placeholder value, set to whatever default speed is.
     return new_speed;
 }
 
-int accelerate(const int current_speed) {
+int accelerate(int current_speed) {
     // Increases motor speed value (0-255) by 10 if possible. Otherwise, motor speed value
     // will be increased to maximum, or an error message will be printed if already at maximum
     // speed.
@@ -42,13 +47,13 @@ int accelerate(const int current_speed) {
         new_speed = 255;
     }
     else {
-        // Increase motor speed value by 5.
+        // Increase motor speed value by 10.
         new_speed += 10;
     }
     return new_speed;
 }
 
-int decelerate(const int current_speed) {
+int decelerate(int current_speed) {
     // Decreases motor speed value (0-255) by 10 if possible, otherwise calls driving_stop().
     int new_speed = current_speed;
     // Might be better to have constant to store the speed change intervals (e.g. at the
@@ -57,11 +62,29 @@ int decelerate(const int current_speed) {
         printf("Error: Motor speed already at zero.\n");
     }
     else if (current_speed <= 10) {
-        new_speed = driving_stop();
+        new_speed = driving_motor_stop();
     }
     else {
-        // Decrease driving motor speed value by 5.
+        // Decrease driving motor speed value by 10.
         new_speed -= 10;
     }
     return new_speed;
+}
+
+int main() {
+    // This is just a test sequence. Actual 
+    driving_motor_setup();
+    while (true) {
+        drive_forwards();
+        sleep_ms(3000);
+
+        driving_motor_stop();
+        sleep_ms(3000);
+
+        drive_forwards();
+        sleep_ms(3000);
+
+        driving_motor_stop();
+        sleep_ms(2000);
+    }
 }
