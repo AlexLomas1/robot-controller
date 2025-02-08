a4c48390094ae1d2097086719bb7719f3bcfd61f
Alex Lomas <alexlomas951@gmail.com>
2025-02-08 15:00:24 +0800

    Readded steer_right and steer_left functions, altered for working with a servo motor

--- steering_motor.c
+++ steering_motor.c
@@ -12,7 +12,7 @@ void steering_motor_setup() {
 void set_servo_angle(int pulse_width) {
     // A pulse is sent with a total length of 20ms. The pulse width (time it spends on) is changed
     // between 0.5 and 2.5ms (500 and 2500us). The pulse width determines the angle of the servo motor,
-    // with 0.5ms as 0 degrees, 1.5ms as 90 degrees (neutral), and 2.5ms as 180 degrees.
+    // with 0.5ms as 90 degrees right, 1.5ms as 0 degrees, and 2.5ms as 90 degrees left.
     gpio_put(STEERING_MOTOR_PIN_1, 1);
     sleep_us(pulse_width); // us used instead of ms as sleep functions only take integer paramters, so 
     // for example 0.5ms can't be done with sleep_ms
@@ -20,17 +20,47 @@ void set_servo_angle(int pulse_width) {
     sleep_us(20000 - pulse_width);
 }
 
+void steer_right(int angle) {
+    // Note that angle is in terms of integer multiples of 11.25 degrees (e.g. 0 = 0 degrees, 2 = 22.5 
+    // degrees), up to 8 (90 degrees).
+    int pulse_width;
+
+    if (angle >= 0 && angle <= 8) {
+        pulse_width = 1500 - (angle * 125);
+        set_servo_angle(pulse_width);
+    }
+}
+
+void steer_left(int angle) {
+    // Note that angle is in terms of integer multiples of 11.25 degrees (e.g. 0 = 0 degrees, 2 = 22.5 
+    // degrees), up to 8 (90 degrees).
+    int pulse_width;
+
+    if (angle >= 0 && angle <= 8) {
+        pulse_width = 1500 + (angle * 125);
+        set_servo_angle(pulse_width);
+    }
+}
+
 void main() {
     steering_motor_setup();
 
-    // Test sequence. Each 250us change in pulse width is about 22.5 degrees of rotation
-    int pulse_width;
-    for (pulse_width = 500; pulse_width <= 2500; pulse_width += 250) {
-        set_servo_angle(pulse_width);
+    // Test sequence.
+    set_servo_angle(1500); // Sets servo to middle / neutral.
+    sleep_ms(1000);
+
+    int i;
+    for (i = 1; i <= 8; i += 1) {
+        steer_left(i);
         sleep_ms(250);
     }
-    for (pulse_width = 2250; pulse_width >= 500; pulse_width -= 250) {
-        set_servo_angle(pulse_width);
+
+    set_servo_angle(1500);
+    sleep_ms(1000);
+
+    for (i = 1; i <= 8; i += 1) {
+        steer_right(i);
         sleep_ms(250);
     }
+    set_servo_angle(1500);
 }
