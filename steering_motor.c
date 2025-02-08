4b77b2186faa7212f101ffbee5565b275342ca9e
Alex Lomas <alexlomas951@gmail.com>
2025-02-08 12:21:48 +0800

    Reworked steering_motor.c to control a servo motor instead of a DC motor

--- steering_motor.c
+++ steering_motor.c
@@ -1,70 +1,36 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
-#include "hardware/pwm.h"
 
-#define STEERING_MOTOR_PIN_1 3 // IN1 (Turn right)
-#define STEERING_MOTOR_PIN_2 4 // IN2 (Turn left)
-#define STEERING_MOTOR_PWM_PIN 15 // ENA (PWM Control for steering motor)
-// NOTE: pins used are currently the same as motor A pins in driving_motor. This is just to make it
-// easier to test code with just one motor.
+#define STEERING_MOTOR_PIN_1 3
 
 void steering_motor_setup() {
-    // Sets up steering motor.
+    // Sets up the servo motor used for steering.
     gpio_init(STEERING_MOTOR_PIN_1);
     gpio_set_dir(STEERING_MOTOR_PIN_1, GPIO_OUT);
-
-    gpio_init(STEERING_MOTOR_PIN_2);
-    gpio_set_dir(STEERING_MOTOR_PIN_2, GPIO_OUT);
-
-    gpio_set_function(STEERING_MOTOR_PWM_PIN, GPIO_FUNC_PWM);
-    uint slice_num = pwm_gpio_to_slice_num(STEERING_MOTOR_PWM_PIN);
-    pwm_set_wrap(slice_num, 255); // Sets PWM range (0-255) to act as speed control for motor.
-    pwm_set_enabled(slice_num, true); // Enable PWM
 }
 
-void set_steering_speed(int new_speed) {
-    // Changes the speed value (0-255) of the motor.
-    // NOTE: testing has shown that current motor doesn't run at a new_speed value of
-    // below 80.
-    uint slice_num = pwm_gpio_to_slice_num(STEERING_MOTOR_PWM_PIN);
-    pwm_set_gpio_level(STEERING_MOTOR_PWM_PIN, new_speed);
-}
-
-void stop_steering() {
-    // Stops steering motor.
-    gpio_put(STEERING_MOTOR_PIN_1, 0);
-    gpio_put(STEERING_MOTOR_PIN_2, 0);
-    set_steering_speed(0);
-}
-
-void steer_right(int turn_duration) {
-    // Steers right.
+void set_servo_angle(int pulse_width) {
+    // A pulse is sent with a total length of 20ms. The pulse width (time it spends on) is changed
+    // between 0.5 and 2.5ms (500 and 2500us). The pulse width determines the angle of the servo motor,
+    // with 0.5ms as 0 degrees, 1.5ms as 90 degrees (neutral), and 2.5ms as 180 degrees.
     gpio_put(STEERING_MOTOR_PIN_1, 1);
-    gpio_put(STEERING_MOTOR_PIN_2, 0);
-    set_steering_speed(100); // Motor not set to full speed for more precise turning
-    sleep_ms(turn_duration); // Turn duration used to change angle of rotation
-    stop_steering();
-}
-
-void steer_left(int turn_duration) {
-    // Steers left.
+    sleep_us(pulse_width); // us used instead of ms as sleep functions only take integer paramters, so 
+    // for example 0.5ms can't be done with sleep_ms
     gpio_put(STEERING_MOTOR_PIN_1, 0);
-    gpio_put(STEERING_MOTOR_PIN_2, 1);
-    set_steering_speed(100); // Motor not set to full speed for more precise turning
-    sleep_ms(turn_duration); // Turn duration used to change angle of rotation
-    stop_steering();
+    sleep_us(20000 - pulse_width);
 }
 
 void main() {
     steering_motor_setup();
-    while (true) {
-        steer_right(10); // Turn right for 10ms
-        sleep_ms(1500);
 
-        steer_left(10); // Turn left for 10ms
-        sleep_ms(3000);
-
-        // PROBLEM: currently the motor steers more when steering right than left. This could be a 
-        // limitation with the motor itself.
+    // Test sequence. Each 250us change in pulse width is about 22.5 degrees of rotation
+    int pulse_width;
+    for (pulse_width = 500; pulse_width <= 2500; pulse_width += 250) {
+        set_servo_angle(pulse_width);
+        sleep_ms(250);
+    }
+    for (pulse_width = 2250; pulse_width >= 500; pulse_width -= 250) {
+        set_servo_angle(pulse_width);
+        sleep_ms(250);
     }
 }
