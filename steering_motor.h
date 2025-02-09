209340ae7946a22ab1213d588038761a104ac787
Alex Lomas <alexlomas951@gmail.com>
2025-02-09 11:10:40 +0800

    Created header file for steering motor

---
+++ steering_motor.h
@@ -0,0 +1,7 @@
+void steering_motor_setup(); // Sets up the servo motor used for steering.
+
+void set_servo_angle(); // Sets the angle of the servo motor with basic PWM control.
+
+void steer_right(); // Steers to the right, up to 90 degrees from neutral.
+
+void steer_left(); // Steers to the left, up to 90 degrees from neutral.
