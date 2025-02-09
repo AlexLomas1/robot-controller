c816acf5c50f27474e6e7f05b7b1705ede38ed6f
Alex Lomas <alexlomas951@gmail.com>
2025-02-09 10:24:36 +0800

    Removed accelerate and decelerate functions from driving motor as unnecessary

--- driving_motor.c
+++ driving_motor.c
@@ -29,82 +29,27 @@ void set_motor_speed(int new_speed) {
     pwm_set_gpio_level(MOTOR_A_PWM_PIN, new_speed);
 }
 
-int driving_motor_stop() {
+void driving_motor_stop() {
     // Stops driving motor.
+    set_motor_speed(0);
     gpio_put(MOTOR_A_PIN_1, 0);
     gpio_put(MOTOR_A_PIN_2, 0);
-    set_motor_speed(0);
-
-    int new_speed = 0;
-    return new_speed;
 }
 
-int drive_forwards() {
+void drive_forwards() {
     // Turns on the driving motor in the forward direction, with the motor speed value 
     // (0-255) initialised to 150.
     gpio_put(MOTOR_A_PIN_1, 1);
     gpio_put(MOTOR_A_PIN_2, 0);
-    set_motor_speed(150); // NOTE: testing has shown that current motor doesn't run at a 
-    // speed value of below 80.
-
-    int new_speed = 150; // Placeholder value, set to whatever default speed is.
-    return new_speed;
+    set_motor_speed(150); // Placeholder value.
 }
 
-int drive_backwards() {
+void drive_backwards() {
     // Turn on the driving motor in the backward direction, with the motor speed value 
     // (0-255) initialised to 150.
     gpio_put(MOTOR_A_PIN_1, 0);
     gpio_put(MOTOR_A_PIN_2, 1);
-    set_motor_speed(150); // NOTE: testing has shown that current motor doesn't run at a 
-    // speed value of below 80.
-
-    int new_speed = 150;
-    return new_speed;
-}
-
-int accelerate(int current_speed) {
-    // Increases motor speed value (0-255) by 10 if possible. Otherwise, motor speed value
-    // will be increased to maximum, or an error message will be printed if already at maximum
-    // speed.
-    int new_speed = current_speed;
-    // Might be better to have constant to store the speed change intervals (e.g. at the
-    // moment speed intervals is 10).
-    if (current_speed == 255) {
-        printf("Already at max speed!\n");
-    }
-    else if (current_speed + 10 >= 255) {
-        // Increase motor speed to 255.
-        new_speed = 255;
-        set_motor_speed(255);
-    }
-    else {
-        // Increase motor speed value by 10.
-        new_speed += 10;
-        set_motor_speed(new_speed);
-    }
-    return new_speed;
-}
-
-int decelerate(int current_speed) {
-    // Decreases motor speed value (0-255) by 10 if possible, otherwise stops motor, or an error
-    // message will be printed if motor already stopped.
-    int new_speed = current_speed;
-    // Might be better to have constant to store the speed change intervals (e.g. at the
-    // moment speed intervals is 10).
-    if (current_speed == 0) {
-        printf("Error: Motor speed already at zero.\n");
-    }
-    else if (current_speed - 10 <= 0) {
-        // Stop motor.
-        new_speed = driving_motor_stop();
-    }
-    else {
-        // Decrease driving motor speed value by 10.
-        new_speed -= 10;
-        set_motor_speed(new_speed);
-    }
-    return new_speed;
+    set_motor_speed(150); // Placeholder value.
 }
 
 int main() {
