3e120756b88567f9752c95046a5e275877994dd5
Alex Lomas <alexlomas951@gmail.com>
2025-02-09 10:36:57 +0800

    Created header file for driving motor

---
+++ driving_motor.h
@@ -0,0 +1,9 @@
+void driving_motor_setup(); // Sets up the driving motor.
+
+void set_motor_speed(); // Sets the motor speed to a value 0-255.
+
+void driving_motor_stop(); // Stops the driving motor.
+
+void drive_forwards(); // Turns on the driving motors in the forward direction.
+
+void drive_backwards(); // Turns on the driving motors in the backwards direction.
