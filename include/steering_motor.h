b2b26ac7bc1832e349c2b87f0832bd17cc525d69
Alex Lomas <alexlomas951@gmail.com>
2025-03-02 11:33:09 +0800

    Added centre_servo function to header file

--- include/steering_motor.h
+++ include/steering_motor.h
@@ -5,3 +5,5 @@ void set_servo_angle(); // Sets the angle of the servo motor with basic PWM cont
 void steer_right(); // Steers to the right, up to 90 degrees from neutral.
 
 void steer_left(); // Steers to the left, up to 90 degrees from neutral.
+
+void centre_servo(); // Centres the servo motor (0 degrees)
