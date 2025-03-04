0a2b08b98b127df620d2250e2b3b6134d9be3953
Alex Lomas <alexlomas951@gmail.com>
2025-03-04 16:58:10 +0800

    Replaced many else if statements with switch case

--- src/main.c
+++ src/main.c
@@ -26,76 +26,77 @@ int main() {
     steering_motor_setup();
     uart_setup();
     uart_puts(uart0, "hello, world\n");
+    int user_input, received_num, i;
 
-    int i;
     while (true) {
         if (uart_is_readable(uart0)) {
             // Reads input from UART
-            int user_input = uart_getc(uart0); // Gets the ASCII value of entered character.
+            user_input = uart_getc(uart0); // Gets the ASCII value of entered character.
 
-            if (user_input == 102) { // ASCII value of f for forwards
-                uart_puts(uart0, "Driving forwards\n");
-                drive_forwards();
-            }
+            switch (user_input) {
+                case 102: // ASCII value of f - Drive forwards
+                    uart_puts(uart0, "Driving forwards\n");
+                    drive_forwards();
+                    break;
+                
+                case 98: // ASCII value of b - Drive backwards
+                    uart_puts(uart0, "Driving backwards\n");
+                    drive_backwards();
+                    break;
 
-            else if (user_input == 98) { // ASCII value of b for backwards
-                uart_puts(uart0, "Driving backwards\n");
-                drive_backwards();
-            }
+                case 99: // ASCII value of c - Change speed
+                    user_input = uart_getc(uart0);
+                    received_num = user_input - 48; // Recieves a number 0 to 9 based on its ASCII value
+                    if (received_num >= 0 && received_num <= 9) {
+                        // Converts received num to a value 80-251, to cover most of the speed values (0-255,
+                        // but testing has shown that motor doesn't run at a speed of below 80).
+                        int new_speed = (received_num * 19) + 80;
+                        uart_puts(uart0, "Changing speed \n");
+                        set_motor_speed(new_speed);
+                    }
+                    else {
+                        uart_puts(uart0, "Invalid speed value\n");
+                    } 
+                    break;
+                
+                case 115: // ASCII value of s - Stop driving
+                    uart_puts(uart0, "Stopping driving motor\n");
+                    driving_motor_stop();
+                    break;
 
-            else if (user_input == 99) { // ASCII value of c for change speed
-                int user_input = uart_getc(uart0);
-                int received_num = user_input - 48; // Recieves a number 0 to 9 based on its ASCII value
-                if (received_num >= 0 && received_num <= 9) {
-                    // Converts received num to a value 80-251, to cover most of the speed values (0-255,
-                    // but testing has shown that motor doesn't run at a speed of below 80).
-                    int new_speed = (received_num * 19) + 80;
-                    uart_puts(uart0, "Changing speed \n");
-                    set_motor_speed(new_speed);
-                }
-                else {
-                    uart_puts(uart0, "Invalid speed value\n");
-                }
-            }
+                case 114: // ASCII value of r - Turn right
+                    user_input = uart_getc(uart0);
+                    received_num = user_input - 48; // Recieves a number 0 to 8 based on its ASCII value
+                    if (received_num >= 0 && received_num <= 8) {
+                        uart_puts(uart0, "Turning right\n");
+                        steer_right(received_num);
+                    }
+                    else {
+                        uart_puts(uart0, "Invalid steering magnitude\n");
+                    }
+                    break;
+                    
+                case 108: // ASCII value of l - Turn left
+                    user_input = uart_getc(uart0);
+                    received_num = user_input - 48; // Recieves a number 0 to 8 based on its ASCII value
+                    if (received_num >= 0 && received_num <= 8) {
+                        uart_puts(uart0, "Turning left\n");
+                        steer_left(received_num);
+                    }
+                    else {
+                        uart_puts(uart0, "Invalid steering magnitude\n");
+                    }
+                    break;
 
-            else if (user_input == 115) { // ASCII value of s for stop
-                uart_puts(uart0, "Stopping driving motor\n");
-                driving_motor_stop();
+                case 109: // ASCII value of m - Centres servo
+                    uart_puts(uart0, "Returning to neutral steering\n");
+                    centre_servo();
+                    break;
+                    
+                default:
+                    uart_puts(uart0, "Unknown input\n");
             }
-
-            else if (user_input == 114) { // ASCII value of r for right
-                int user_input = uart_getc(uart0);
-                int received_num = user_input - 48; // Recieves a number 0 to 8 based on its ASCII value
-                if (received_num >= 0 && received_num <= 8) {
-                    uart_puts(uart0, "Turning right\n");
-                    steer_right(received_num);
-                }
-                else {
-                    uart_puts(uart0, "Invalid steering magnitude\n");
-                }
-            }
-
-            else if (user_input == 108) { // ASCII value of l for left
-                int user_input = uart_getc(uart0);
-                int received_num = user_input - 48; // Recieves a number 0 to 8 based on its ASCII value
-                if (received_num >= 0 && received_num <= 8) {
-                    uart_puts(uart0, "Turning left\n");
-                    steer_left(received_num);
-                }
-                else {
-                    uart_puts(uart0, "Invalid steering magnitude\n");
-                }
-            }
-
-            else if (user_input == 109) { // ASCII value of m for middle
-                uart_puts(uart0, "Returning to neutral steering\n");
-                steer_left(0);
-            }
-
-            else {
-                uart_puts(uart0, "Unknown input\n");
-            }
+            sleep_ms(250); // Delay to prevent multiple actions being attempted at once.
         }
-        sleep_ms(250); // Delay to prevent multiple actions being attempted at once.
     }
 }          
