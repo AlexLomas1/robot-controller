16c95a870ecd8f8a78720d8e27e9e12a988dd285
Alex Lomas <alexlomas951@gmail.com>
2025-03-08 12:54:22 +0800

    Replaced bit banging approach to servo motor control with using actual PWM hardware

--- src/steering_motor.c
+++ src/steering_motor.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "hardware/pwm.h"
 #include "../include/steering_motor.h"
 
 #define STEERING_MOTOR_PIN 10
 
 void steering_motor_setup() {
     // Sets up the servo motor used for steering.
-    gpio_init(STEERING_MOTOR_PIN);
-    gpio_set_dir(STEERING_MOTOR_PIN, GPIO_OUT);
+    gpio_set_function(STEERING_MOTOR_PIN, GPIO_FUNC_PWM);
+    uint slice_num = pwm_gpio_to_slice_num(STEERING_MOTOR_PIN);
+    pwm_set_clkdiv(slice_num, 64.0f); // Sets PWM clock divider, 
+    pwm_set_wrap(slice_num, 39062); // Sets PWM counter wrap value to 39062.
+    // The frequency of the PWM is calculated as (system clock) / (clkdiv * (wrap + 1)), as
+    // the PWM counts from 0 to the wrap value, and the clkdiv, as the name implies, divides the system
+    // clock. The default system clock for the Pico is 125MHz, so frequency is 125MHz / (64 * 39063),
+    // which is approximately 50Hz (or period of 20ms), which is what typical servo motors require.
+    pwm_set_enabled(slice_num, true); // Enable PWM for steering motor
 }
 
 void set_servo_angle(int pulse_width) {
@@ -15,16 +23,11 @@ void set_servo_angle(int pulse_width) {
     // between 0.5 and 2.5ms (500 and 2500us). The pulse width determines the angle of the servo motor,
     // with 0.5ms as 90 degrees right, 1.5ms as 0 degrees, and 2.5ms as 90 degrees left.
 
-    // Note that while only one pulse needs to be sent, sending multiple seems to make the angle 
-    // more consistent, especially when moving over larger angles in one go.
-    int i;
-
-    for (i = 0; i <= 10; i+= 1) {
-        gpio_put(STEERING_MOTOR_PIN, 1);
-        sleep_us(pulse_width); 
-        gpio_put(STEERING_MOTOR_PIN, 0);
-        sleep_us(20000 - pulse_width);
-    }
+    uint slice_num = pwm_gpio_to_slice_num(STEERING_MOTOR_PIN);
+    // Pulse width (us) is converted to PWM level. As PWM wrap value is 39062, full 20ms period is
+    // 39062, so PWM level is calculated as below.
+    int level = (pulse_width / 20000) * 39062;
+    pwm_set_gpio_level(STEERING_MOTOR_PIN, level);
 }
 
 void centre_servo() {
@@ -34,7 +37,7 @@ void centre_servo() {
 
 void steer_right(int angle) {
     // Note that angle is in terms of integer multiples of 11.25 degrees (e.g. 0 = 0 degrees, 2 = 22.5 
-    // degrees), up to 8 (90 degrees).
+    // degrees), up to 8 (90 degrees). 
     int pulse_width;
 
     if (angle >= 0 && angle <= 8) {
@@ -45,7 +48,7 @@ void steer_right(int angle) {
 
 void steer_left(int angle) {
     // Note that angle is in terms of integer multiples of 11.25 degrees (e.g. 0 = 0 degrees, 2 = 22.5 
-    // degrees), up to 8 (90 degrees).
+    // degrees), up to 8 (90 degrees). 
     int pulse_width;
 
     if (angle >= 0 && angle <= 8) {
