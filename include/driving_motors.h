577d79dfa8000199c379bd46799dbf618b112ab5
Alex Lomas <alexlomas951@gmail.com>
2025-03-09 09:28:48 +0800

    Improved comments and created default speed constant

--- include/driving_motors.h
+++ include/driving_motors.h
@@ -1,9 +1,14 @@
-void driving_motors_setup(); // Sets up the driving motors.
+// Initialises GPIO pins used to control DC motors, and configures PWM for speed control.
+void driving_motors_setup();
 
-void set_motor_speed(); // Sets the motor speed to a value 0-255.
+// Sets the motor speed (0-255). Note: speed values of below 80 may not produce movement.
+void set_motor_speed(int new_speed);
 
-void driving_stop(); // Stops the driving motors.
+// Stops driving motors by setting all pins to low, with motor speed set to 0 for safety.
+void driving_stop();
 
-void drive_forwards(); // Turns on the driving motors in the forward direction.
+// Turns on the driving motors in the forward direction at the default speed.
+void drive_forwards(); 
 
-void drive_backwards(); // Turns on the driving motors in the backwards direction.
+// Turns on the driving motors in the backwards direction at the default speed.
+void drive_backwards(); 
--- src/driving_motors.c
+++ src/driving_motors.c
@@ -11,8 +11,10 @@
 #define MOTOR_B_PIN_2 5 // Backwards for Motor B
 #define MOTOR_B_PWM_PIN 15 // PWM Control for Motor B
 
+#define DEFAULT_SPEED 150
+
 void driving_motors_setup() {
-    // Sets up the driving motors.
+    // Initialises GPIO pins used to control DC motors, and configures PWM for speed control.
     
     gpio_init(MOTOR_A_PIN_1);
     gpio_set_dir(MOTOR_A_PIN_1, GPIO_OUT);
@@ -27,26 +29,25 @@ void driving_motors_setup() {
     gpio_set_dir(MOTOR_B_PIN_2, GPIO_OUT);
 
     gpio_set_function(MOTOR_A_PWM_PIN, GPIO_FUNC_PWM);
-    uint slice_num = pwm_gpio_to_slice_num(MOTOR_A_PWM_PIN);
-    pwm_set_wrap(slice_num, 255); // Sets PWM range (0-255) to act as speed control for motor A.
-    pwm_set_enabled(slice_num, true); // Enable PWM for motor A.
+    uint slice_num_a = pwm_gpio_to_slice_num(MOTOR_A_PWM_PIN);
+    pwm_set_wrap(slice_num_a, 255); // Sets PWM counter wrap value to 255 for Motor A.
+    pwm_set_enabled(slice_num_a, true); // Enable PWM for motor A.
 
     gpio_set_function(MOTOR_B_PWM_PIN, GPIO_FUNC_PWM);
-    slice_num = pwm_gpio_to_slice_num(MOTOR_B_PWM_PIN);
-    pwm_set_wrap(slice_num, 255); // Sets PWM range (0-255) to act as speed control for motor B.
-    pwm_set_enabled(slice_num, true); // Enable PWM for motor B.
+    uint slice_num_b = pwm_gpio_to_slice_num(MOTOR_B_PWM_PIN);
+    pwm_set_wrap(slice_num_b, 255); // Sets PWM counter wrap value to 255 for Motor B.
+    pwm_set_enabled(slice_num_b, true); // Enable PWM for motor B.
 }
 
 void set_motor_speed(int new_speed) {
-    // Changes the speed value (0-255) of the motors.
-    // Note: testing has shown that current motor doesn't run at a new_speed value of
-    // below 80. 
+    /* Sets the speed value (0-255) of the motors. Note: testing has shown that current motor 
+    doesn't run at a new_speed value of below 80. */
     pwm_set_gpio_level(MOTOR_A_PWM_PIN, new_speed);
     pwm_set_gpio_level(MOTOR_B_PWM_PIN, new_speed);
 }
 
 void driving_stop() {
-    // Stops driving motors.
+    // Stops driving motors by setting all pins to low, with motor speed set to 0 for safety.
     gpio_put(MOTOR_A_PIN_1, 0);
     gpio_put(MOTOR_A_PIN_2, 0);
     gpio_put(MOTOR_B_PIN_1, 0);
@@ -55,21 +56,21 @@ void driving_stop() {
 }
 
 void drive_forwards() {
-    // Turns on the driving motors in the forward direction, with the motor speed value 
-    // (0-255) initialised to 150.
+    /* Turns on the driving motors in the forwards direction, with the motor speed value (0-255)
+    set to the default speed value. */
     gpio_put(MOTOR_A_PIN_1, 1);
     gpio_put(MOTOR_A_PIN_2, 0);
     gpio_put(MOTOR_B_PIN_1, 1);
     gpio_put(MOTOR_B_PIN_2, 0);
-    set_motor_speed(150); 
+    set_motor_speed(DEFAULT_SPEED); 
 }
 
 void drive_backwards() {
-    // Turn on the driving motors in the backward direction, with the motor speed value 
-    // (0-255) initialised to 150.
+    /* Turns on the driving motors in the backwards direction, with the motor speed value (0-255) 
+    set to the default speed value. */
     gpio_put(MOTOR_A_PIN_1, 0);
     gpio_put(MOTOR_A_PIN_2, 1);
     gpio_put(MOTOR_B_PIN_1, 0);
     gpio_put(MOTOR_B_PIN_2, 1);
-    set_motor_speed(150);
+    set_motor_speed(DEFAULT_SPEED);
 }
